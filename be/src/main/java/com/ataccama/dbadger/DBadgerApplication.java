8ce0de254c5cb04ce6ef643f8f9b771d5ed951ff
Darius Kryszczuk <darius.kryszczuk@gmail.com>
2021-06-15 19:40:08 +0200

    Project setup
    Project setup via Spring Initializr.

---
+++ be/src/main/java/com/ataccama/dbadger/DBadgerApplication.java
@@ -0,0 +1,13 @@
+package com.ataccama.dbadger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBadgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBadgerApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/com/ataccama/dbadger/DbadgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ataccama.dbadger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbadgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
